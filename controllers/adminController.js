4c3d695b9b4ac4c8fb054f7622e02e092650e6c1
Albin Baby <albinbtg@gmail.com>
2023-11-08 14:14:04 +0530

    puppeteer

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1907,7 +1907,7 @@ const salesReportInPdf = async (req, res, next) => {
 
         let endingDate = req.query.endingDate;
 
-        const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"] });
+        const browser = await puppeteer.launch({ headless: 'new', args: ["--no-sandbox", "--disable-setuid-sandbox"] });
         const page = await browser.newPage();
 
 
